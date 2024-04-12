6eed5ca9caa53ee6d939f8987a6ceab3f82b5177
likai <likaiprime@gmail.com>
2024-04-12 12:21:08 +0800

    提交配置

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,7 +2,7 @@ export default function () {
   return (
     <>
       <h1 className="my-5 text-center text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl text-primary">
-        Suno Music Generator
+        Sky Music AI Generator
       </h1>
       <h2 className="mx-auto mb-4 text-center text-sm text-[#636262] sm:h2x-8 sm:text-xl md:px-24 lg:mb-4">
         Generate captivating Music with AI
