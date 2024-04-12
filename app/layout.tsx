727945d762603e76505d77117f318fa6c9880313
likai <likaiprime@gmail.com>
2024-04-12 21:07:08 +0800

    提交配置

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: "Sky Music AI Generator",
   description: "Harness the power of suno.ai's music generation AI by leveraging its API to create captivating melodies.",
   keywords: "Suno, Suno AI, Music Generator, Sky Music AI Generator",
-  creator: "Alvin-Liu",
+  creator: "LeePro",
 };
 
 export default function RootLayout({
@@ -27,18 +27,19 @@ export default function RootLayout({
           <Toaster position="top-center" richColors />
 
           {children}
-          <Script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
-          />
-          <Script id="google-analytics">
-            {`window.dataLayer = window.dataLayer || [];
+
+        </body>
+        <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+        />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
           `}
-          </Script>
-        </body>
+        </Script>
       </html>
     </ClerkProvider>
   );
